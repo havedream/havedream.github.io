51a34b00a97a52a300f9d586bd78c11ec67081e1
havedream <havedream.in@yandex.ru>
2019-12-26 17:32:46 +0300

    autotile for c2

---
+++ tools/c2autotile/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1577370632,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/frame.png",
+		"images/edbg.png",
+		"images/cursor-sheet0.png",
+		"images/tile10x10.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
