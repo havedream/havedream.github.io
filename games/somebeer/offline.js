d890b21bdc6005e9c090fdbd33b33087917bc985
havedream <havedream.in@yandex.ru>
2020-10-03 21:43:34 +0300

    sb
    -

---
+++ games/somebeer/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1601749782,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"i/bg.png",
+		"i/beerman-sheet0.png",
+		"i/drinker-sheet0.png",
+		"i/drinker-sheet1.png",
+		"i/glass-sheet0.png",
+		"i/ground-sheet0.png",
+		"i/particles.png",
+		"i/fullbeer-sheet0.png",
+		"i/x-sheet0.png",
+		"i/kran-sheet0.png",
+		"i/beer-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
